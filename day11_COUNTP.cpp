9f4fbbe19b94ed6e04f7c2a10f1aacec66e30bc7
AMIT KUSHWAH <145380632+7610490182@users.noreply.github.com>
2024-10-08 23:09:17 +0530

    Add files via upload

---
+++ day11_COUNTP.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T, N, count;
+
+    cin >> T;
+
+    
+    for(int i=0;i<T;i++) {
+        
+        count = 0;            // stores the no. of odd elements in complete array
+        
+        cin >> N;
+        int arr[N];
+        
+        for(int j = 0 ; j < N ; j++) {       // input the elements into array
+            cin >> arr[j];
+        }
+        
+        for(int j = 0; j < N; j++) {       
+            if(arr[j] % 2 != 0)              
+                count++;
+        }
+        
+        if(count % 2 != 0 || count == 0)          
+            cout<< "NO"<<endl;
+        else
+            cout << "YES"<<endl;
+    }
+    
+    return 0;
+}
