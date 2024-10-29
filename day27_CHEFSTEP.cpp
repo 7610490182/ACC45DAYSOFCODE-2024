87139b2221181830ce3d6514fa1c3cbd1eac539b
AMIT KUSHWAH <145380632+7610490182@users.noreply.github.com>
2024-10-29 21:45:09 +0530

    Add files via upload

---
+++ day27_CHEFSTEP.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n;
+
+	cin>>n;
+
+	while(n--){
+	    int m , a;
+	    cin>>m>>a;
+	    string s;
+	    for(int i=0; i<m ; i++){
+	        int e;
+	        cin>>e;
+	        
+	        if(e%a==0){
+	          s.push_back('1');
+	        }
+	        else{
+	           s.push_back('0');
+	        }
+	    }
+	    
+	    cout<<s<<endl;
+	}
+}
---
+++ day27_PRB01.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t; cin>>t;
+
+	while(t--){
+
+	    int n, i; cin>>n;
+	    for( i=2; i<n; i++ ){
+	        if(n%i == 0){
+	            break;
+	        }
+	    }
+	    
+	    if( i == n)
+	        cout<<"yes"<<endl;
+	    else
+	        cout<<"no"<<endl;
+	    
+	}
+	return 0;
+}
