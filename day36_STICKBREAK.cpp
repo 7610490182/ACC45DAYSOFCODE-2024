92f6a5edf171a0bc16729b9d11651fbfdc00e55d
AMIT KUSHWAH <145380632+7610490182@users.noreply.github.com>
2024-11-07 22:47:19 +0530

    Add files via upload

---
+++ day36_STICKBREAK.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+  int t;  cin>>t;
+
+    while(t--){
+
+        int l,k;    cin>>l>>k;
+        if(l%k==0)  cout<<0<<endl;
+        else        cout<<1<<endl;
+    }
+
+    
+}
