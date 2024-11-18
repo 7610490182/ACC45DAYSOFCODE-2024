09b96ab3cdbb00c74938e14d9f68913dab3fed57
AMIT KUSHWAH <145380632+7610490182@users.noreply.github.com>
2024-11-18 22:35:43 +0530

    Add files via upload

---
+++ day47_AIRLINE.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+
+	cin>>t;
+
+	while(t--){
+	    int a,b,c,d,e;
+	    cin>>a>>b>>c>>d>>e;
+	    int sum1=a+b;
+	    int sum2=b+c;
+	    int sum3=c+a;
+	    if((sum1<=d && c<=e) || (sum2<=d && a<=e) || (sum3<=d && b<=e)){
+	        std::cout << "YES" << std::endl;
+	    }
+	    else{
+	        std::cout << "NO" << std::endl;
+	    }
+	}
+	return 0;
+}
